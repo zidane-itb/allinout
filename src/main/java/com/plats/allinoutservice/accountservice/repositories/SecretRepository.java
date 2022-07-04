dea86ccc1b3040be50db3c08e549aec840a63130
zidane-itb <89019288+zidane-itb@users.noreply.github.com>
2022-07-04 15:47:17 +0700

    added active into consideration in existsSecret checking method.

--- src/main/java/com/plats/allinoutservice/accountservice/repositories/SecretRepository.java
+++ src/main/java/com/plats/allinoutservice/accountservice/repositories/SecretRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface SecretRepository extends CrudRepository<Secret, String> {
 
-    boolean existsSecretByUsername(String username);
+    boolean existsSecretByUsernameAndActiveIsTrue(String username);
 }
