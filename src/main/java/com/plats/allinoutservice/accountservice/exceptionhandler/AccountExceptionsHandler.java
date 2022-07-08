e1083fb10714eceb02f78e0c558eec7ec368cecd
zidane-itb <89019288+zidane-itb@users.noreply.github.com>
2022-07-09 00:50:21 +0700

    added Log4j2 with lombok and more exception handlers.

--- src/main/java/com/plats/allinoutservice/accountservice/exceptionhandler/AccountExceptionsHandler.java
+++ src/main/java/com/plats/allinoutservice/accountservice/exceptionhandler/AccountExceptionsHandler.java
@@ -1,8 +1,9 @@
 package com.plats.allinoutservice.accountservice.exceptionhandler;
 
-import com.plats.allinoutservice.accountservice.exceptions.AccountCredentialsNotAvailableException;
-import com.plats.allinoutservice.accountservice.exceptions.WrongAccountCredentialsException;
+import com.plats.allinoutservice.accountservice.exceptions.*;
 import com.plats.allinoutservice.accountservice.pojo.ErrorResponse;
+import com.plats.allinoutservice.accountservice.pojo.Secret;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,31 +11,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.UUID;
-import java.util.logging.Logger;
 
 @RestControllerAdvice
+@Log4j2
 public class AccountExceptionsHandler {
-
-    private Logger logger = Logger.getLogger(AccountExceptionsHandler.class.getName());
 
     @ExceptionHandler(WrongAccountCredentialsException.class)
     public ResponseEntity<ErrorResponse> wrongAccountCredentialsResponse(Exception ex) {
         logError(ex);
         ErrorResponse errorResponse = new ErrorResponse("Wrong account credentials.");
 
-        String requestId = UUID.randomUUID().toString();
-
-        return exceptionsReturn(requestId, errorResponse);
-    }
-
-    @ExceptionHandler(AccountCredentialsNotAvailableException.class)
-    public ResponseEntity<ErrorResponse> unidentifiedCredentialsErrorResponse(Exception ex) {
-        logError(ex);
-        ErrorResponse errorResponse = new ErrorResponse("Account credentials not available.");
-
-        String requestId = UUID.randomUUID().toString();
-
-        return exceptionsReturn(requestId, errorResponse);
+        return exceptionsReturn(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(DataAccessException.class)
@@ -42,20 +29,51 @@ public class AccountExceptionsHandler {
         logError(ex);
         ErrorResponse errorResponse = new ErrorResponse("Unknown data error.");
 
-        String requestId = UUID.randomUUID().toString();
-        System.out.println(ex);
-
-        return exceptionsReturn(requestId, errorResponse);
+        return exceptionsReturn(errorResponse,HttpStatus.BAD_REQUEST);
     }
 
-    private ResponseEntity<ErrorResponse> exceptionsReturn(String requestId, ErrorResponse errorResponse) {
+    @ExceptionHandler(AccountInvalidException.class)
+    public ResponseEntity<ErrorResponse> accountInvalidExceptionResponse(Exception ex) {
+        logError(ex);
+        ErrorResponse errorResponse = new ErrorResponse("Account secret string invalid.");
+
+        return exceptionsReturn(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(AlreadyLoggedInException.class)
+    public ResponseEntity<ErrorResponse> alreadyLoggedInExceptionResponse(Exception ex) {
+        logError(ex);
+        ErrorResponse errorResponse = new ErrorResponse("Account you try to login already " +
+                                                                "have an active session.");
+
+        return exceptionsReturn(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(NotAdminException.class)
+    public ResponseEntity<ErrorResponse> notAdminExceptionResponse(Exception ex) {
+        logError(ex);
+        ErrorResponse errorResponse = new ErrorResponse("You're accessing admin feature without admin access.");
+
+        return exceptionsReturn(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(SecretStringExpiredException.class)
+    public ResponseEntity<ErrorResponse> secretStringExpiredExceptionResponse(Exception ex) {
+        logError(ex);
+        ErrorResponse errorResponse = new ErrorResponse("You're using an expired session.");
+
+        return exceptionsReturn(errorResponse, HttpStatus.FORBIDDEN);
+    }
+
+    private ResponseEntity<ErrorResponse> exceptionsReturn(ErrorResponse errorResponse, HttpStatus status) {
+        String requestId = UUID.randomUUID().toString();
         return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
+                .status(status)
                 .header("request_id", requestId)
                 .body(errorResponse);
     }
 
     private void logError(Exception ex) {
-        logger.warning("Error detected, exceptions: " + ex.getClass().getName());
+        log.warn("Error detected, exceptions: " + ex.getClass().getName());
     }
 }
